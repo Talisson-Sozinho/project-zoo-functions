983920e0ef49163565f595b529d5fa36f94d1fef
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-14 16:51:28 -0300

    feito requisito 01 - função "getSpeciesByIds" implementada

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return species.filter(({ id }) => ids.some((current) => id === current));
 }
 
 module.exports = getSpeciesByIds;
