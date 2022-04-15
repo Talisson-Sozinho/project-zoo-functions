5f629b2c5a9951c8567214264c517eff139651f2
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-15 18:03:12 -0300

    feito requisito 06 - função "calculateEntry" implementada

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,33 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
+
+const verifyAge = (age) => {
+  if (age < 18) {
+    return 'child';
+  }
+  if (age < 50) {
+    return 'adult';
+  }
+  return 'senior';
+};
 
 function countEntrants(entrants) {
-  // seu código aqui
-  // primeiro commit
+  return entrants.reduce((acc, { age }) => {
+    const classification = verifyAge(age);
+    acc[classification] += 1;
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+
+  const { child, adult, senior } = countEntrants(entrants);
+
+  const amountChildValue = child * prices.child;
+  const amountAdultValue = adult * prices.adult;
+  const amountSeniorValue = senior * prices.senior;
+
+  return amountChildValue + amountAdultValue + amountSeniorValue;
 }
 
 module.exports = { calculateEntry, countEntrants };
