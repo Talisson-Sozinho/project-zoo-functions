8569c8bd325ce5efc365b82ecf728ede024374e3
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-15 20:59:11 -0300

    feito requisito 07 - função "getAnimalMap" implementada

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,31 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const addLocation = (objOfLocation, location, animal) => {
+  const obj = objOfLocation;
+  if (obj[location] === undefined) obj[location] = [];
+  obj[location].push(animal);
+};
+
+const animalMapWithoutNames = () => species.reduce((acc, { location, name }) => {
+  addLocation(acc, location, name);
+  return acc;
+}, {});
+
+const animalMapWithNames = (options) => species.reduce((acc, { location, name, residents }) => {
+  const animal = {};
+  if (options.sex) {
+    const animalFiltered = residents.filter(({ sex: currentSex }) => currentSex === options.sex);
+    animal[name] = animalFiltered.map(({ name: nameAnimal }) => nameAnimal);
+  } else {
+    animal[name] = residents.map(({ name: nameAnimal }) => nameAnimal);
+  }
+  if (options.sorted) animal[name].sort();
+  addLocation(acc, location, animal);
+  return acc;
+}, {});
 
 function getAnimalMap(options) {
-  // seu código aqui
+  return (options && options.includeNames) ? animalMapWithNames(options) : animalMapWithoutNames();
 }
 
 module.exports = getAnimalMap;
