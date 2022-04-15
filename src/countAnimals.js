5a3d985bd99aa653d9f8e696e4c99e5d461aa371
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-14 23:23:58 -0300

    feito requisito 05 - função "countAnimals" implementada

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+function addAnimalAndQuantity(acc, { name, residents }) {
+  const objectAnimals = acc;
+  objectAnimals[name] = residents.length;
+  return objectAnimals;
+}
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce(addAnimalAndQuantity, {});
+  }
+  const { residents } = species.find(({ name }) => name === animal.specie);
+  if (!animal.sex) {
+    return residents.length;
+  }
+  return residents.reduce((acc, { sex }) => (animal.sex === sex ? acc + 1 : acc), 0);
 }
 
 module.exports = countAnimals;
